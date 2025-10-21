db3cdcbaf5cb749f5e6eb694711b97cfbb3a286d
penalife-armadin <evan.pena@armadin.com>
2025-10-21 00:08:08 -0500

    Update reflection_graphs.py
    changed reflection model to be gpt-4-turbo

--- eaia/reflection_graphs.py
+++ eaia/reflection_graphs.py
@@ -148,8 +148,8 @@ class MultiMemoryInput(MessagesState):
 
 
 async def determine_what_to_update(state: MultiMemoryInput):
-    reflection_model = ChatOpenAI(model="gpt-4o", disable_streaming=True)
-    reflection_model = ChatAnthropic(model="claude-3-5-sonnet-latest")
+    reflection_model = ChatOpenAI(model="gpt-4-turbo", disable_streaming=True)
+    #reflection_model = ChatAnthropic(model="claude-3-5-sonnet-latest")
     trajectory = get_trajectory_clean(state["messages"])
     types_of_prompts = "\n".join(
         [f"`{p_type}`: {MEMORY_TO_UPDATE[p_type]}" for p_type in state["prompt_types"]]
