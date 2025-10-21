7a043a2a293d7db7e2c24013018318338e2f561e
Evan Pena <evan.pena@armadin.com>
2025-10-21 01:11:49 -0500

    fixed import

--- eaia/gmail.py
+++ eaia/gmail.py
@@ -16,7 +16,7 @@ import email.utils
 from langchain_auth import Client
 
 from langchain_core.tools import tool
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 
 from eaia.schemas import EmailData
 
